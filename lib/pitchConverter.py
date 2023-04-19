bfe199dadebc788bf989fb039f17b7ff0455941d
newkincode <maainnewkin59@gmail.com>
2023-04-19 23:11:16 +0900

    모르겠다...

--- lib/pitchConverter.py
+++ lib/pitchConverter.py
@@ -5,13 +5,24 @@ def midiToPitch(note, octave):
     midiNum = (octave + 1) * 12 + (note - 1)
     pitch = 440 * pow(2, (midiNum - 69) / 12)
     return pitch
+def note(name):
+    octave = int(name[-1])
+    PITCHES = "c,c#,d,d#,e,f,f#,g,g#,a,a#,b".split(",")
+    pitch = PITCHES.index(name[:-1].lower())
+    return 440 * 2 ** ((octave - 4) + (pitch - 9) / 12.)
+
+def merge_mp3_files(file_list, output_file):
+    combined = AudioSegment.empty()
+    for file in file_list:
+        sound = AudioSegment.from_mp3(file)
+        combined += sound
+    combined.export(output_file, format="mp3")
 
 def pitchConverter(text):
-    with open("tsmmidi.json", "r") as f:
+    with open("tsmmidi", "r") as f:
         midiFile = json.load(f)
 
-    for i in text:
-        for j in midiFile:
-            originalFile = AudioSegment.from_mp3(f"ttsMp3/{i}.mp3")
+    for i, j in zip(text, midiFile):
+            originalFile = AudioSegment.from_mp3(f"ttsMp3\\{i}.mp3")
             conversionFile = originalFile.pitch_semitones(midiToPitch(int(j[0]),int(j[1])))
             conversionFile.export(f"pitchCorrection/{i}.mp3", format="mp3")
